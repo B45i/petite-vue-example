b587715e73cacdc743e64d93fce2d30f76f08a12
B45i <amalshajan2011@gmail.com>
2021-09-29 17:51:02 +0530

    fetching and displying users

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import { createApp } from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js';
 
+const UserCard = user => ({
+    $template: '#user-template',
+
+    // user: user, not needed, user props dirctly
+
+    follow() {
+        this.user.isFollowed = !this.user.isFollowed;
+    },
+});
+
 async function getUsers() {
     const resp = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await resp.json();
@@ -20,4 +30,5 @@ const App = () => {
 
 createApp({
     App,
+    UserCard,
 }).mount();
